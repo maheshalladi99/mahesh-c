29a80c13a3ba386945c6b948cb62695b75799f6d
Mahesh Alladi <alladimaheshbabu333@gmail.com>
2019-03-05 21:52:48 +0530

    Update STRCT1.C

--- STRCT1.C
+++ STRCT1.C
@@ -7,7 +7,7 @@ void main()
 	struct point p1;
 	clrscr();
 	p1.x,p1.a=10;
-	p1.y=20;
+	p1.y=200;
 	p1.z=30;
 	printf("x=%d y=%d z=%d a=%d\n",p1.x,p1.y,p1.z,p1.a);
 	printf("x=%d y=%d z=%d\n",p2.x,p2.y,p2.z);
