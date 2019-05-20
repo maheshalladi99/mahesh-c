c5aa54eba574bdb5c50f611f004882cfd70eac13
Mahesh Alladi <alladimaheshbabu333@gmail.com>
2019-05-20 22:23:47 +0530

    Update 802.C

--- 802.C
+++ 802.C
@@ -3,7 +3,7 @@ void main()
 	int k,i,j;
 	
 	clrscr();
-	printf("please enter a value\n");
+	printf("please enter a value : \n");
 	scanf("%d",&k);
 	printf("the chukka formation is like as follows\n");
 	for(i=1;i<=k;i++)
